2c76c1284c1c8e812460c2c00a12b8974ccf69bb
Adam Rennie <a.rennie1@nhs.net>
2020-09-18 09:11:59 +0100

    Added 2 lines of text code as a test

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 # Description - Describe what the app does (e.g. visualizes births data)
 # User interface - how your app looks and elements users can interact with
-
+# Added line of code for traning purposes
+# Added test
 
 shinyUI(
   
